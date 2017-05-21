499b45272f842463ef43bed621f761b5055eaa81
Phillip Lemons <philliplemons512@gmail.com>
2017-05-21 11:56:20 -0500

    Initial commit
    Contains libraries that my project uses, the initial files required for
    Google App Engine, and a single post from my old blog. The old post has
    been converted into raw html and javascript to work without Wordpress.

---
+++ main.py
@@ -0,0 +1,19 @@
+import logging
+
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+# Old blog redirect
+@ app.route('/2014/09/14/ray-casting-algorithm')
+def ray_casting_algorithm_redirect():
+    return redirect('/posts/ray-casting-algorithm', code=302)
+
+@app.route('/posts/ray-casting-algorithm')
+def ray_casting_algorithm():
+    return render_template("ray-casting-algorithm.html")
+
