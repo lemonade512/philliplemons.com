414cc11275f335dd006ea8f8d98bcbe073dda39b
Phillip Lemons <philliplemons512@gmail.com>
2017-05-24 21:34:18 -0500

    Add "No Cheating Allowed!!!" post

--- main.py
+++ main.py
@@ -12,12 +12,23 @@ def index():
 def about():
     return render_template("about.html")
 
-# Old blog redirect
-@ app.route('/2014/09/14/ray-casting-algorithm')
-def ray_casting_algorithm_redirect():
-    return redirect('/posts/ray-casting-algorithm', code=302)
+@app.route('/posts/no-cheating-allowed')
+def no_cheating_allowed():
+    return render_template("no-cheating-allowed.html")
 
 @app.route('/posts/ray-casting-algorithm')
 def ray_casting_algorithm():
     return render_template("ray-casting-algorithm.html")
+
+##########################################################
+#                 Old Blog Redirects                     #
+##########################################################
+@app.route('/2014/09/14/ray-casting-algorithm')
+def ray_casting_algorithm_redirect():
+    return redirect('/posts/ray-casting-algorithm', code=302)
+
+@app.route('/2015/08/18/no-cheating-allowed')
+def no_cheating_allowed_redirect():
+    return redirect('/posts/no-cheating-allowed', code=302)
+
 
