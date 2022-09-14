a6962669ec1976db378086d30a6e6a1a2b1268f4
Phillip Lemons <philliplemons512@gmail.com>
2022-09-13 22:12:29 -0500

    Add animeJS demo to Vue app

--- src/main/vue/src/views/AboutView.vue
+++ src/main/vue/src/views/AboutView.vue
@@ -1,4 +1,9 @@
+<script setup>
+import Header from '../components/Header.vue'
+</script>
+
 <template>
+  <Header />
   <div class="about">
     <h1>This is an about page</h1>
   </div>
---
+++ src/main/vue/src/views/DemoView.vue
@@ -0,0 +1,47 @@
+<script>
+import anime from 'animejs/lib/anime.es.js';
+
+export default {
+  mounted() {
+    // Wrap every letter in a span
+    var textWrappers = document.querySelectorAll('.hiddenText');
+    for (let i = 0; i < textWrappers.length; i++) {
+      var textWrapper = textWrappers[i];
+      textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    }
+
+    setTimeout(() => {
+      anime.timeline()
+        .add({
+          targets: '.a1 .letter',
+          opacity: 1,
+          delay: (el, i) => 70 * i
+        }).add({
+          targets: '.a2 .letter',
+          opacity: 1,
+          delay: (el, i) => 70 * i
+        }, '+= 300')
+    }, 1500);
+  }
+}
+</script>
+
+<template>
+  <main>
+    <h1 class="a1 hiddenText">Oh, hello!</h1>
+    <h1 class="a2 hiddenText">What is your name?</h1>
+  </main>
+</template>
+
+<style>
+.circle {
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  background-color: red;
+}
+
+.letter {
+  opacity: 0;
+}
+</style>
